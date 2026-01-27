6dd50fbf8d854a87072de5b26f9e11c9520e25cb
Kirigo <mercyndungukirigo@gmail.com>
2026-01-27 16:22:42 +0200

    Female employees that earn more than the company averag

---
+++ Queries.sql
@@ -0,0 +1,31 @@
+-- State which DB i am using
+USE practice_db;
+-- The following SQL qeuries will;
+-- 1. Find all female employees earning more than the company average
+-- 2. List departments with more than 3 employees
+-- 3. Find employees who have been with the company longer than 10 years but have never promoted
+-- 4. Calculate the salary difference between each employee and their department's average
+-- 5. Find pairs of employees with the same education level and similar salaries (within 500)
+
+-- Exercise One: Find all Female employees earning more than the company average.
+SELECT 
+employee_id, age, job_role, department, monthly_salary,
+-- First get the average
+(SELECT AVG(monthly_salary) from Employees) as monthly_avg
+-- Get female employees paid more than the average
+FROM Employees
+WHERE gender = "Female" AND monthly_salary > (SELECT avg(monthly_salary) from Employees)
+ORDER BY monthly_salary DESC;
+
+
+
+    
+	
+
+	
+
+
+
+
+
+
